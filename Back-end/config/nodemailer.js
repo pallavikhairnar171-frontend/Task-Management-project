b79233cfa9fabcb18d00777a486c6cf4212c40fb
PallaviMobicloud1137 <pallavi@mobicloud.co.in>
2026-03-25 18:25:00 +0530

    change the email service

--- Back-end/config/nodemailer.js
+++ Back-end/config/nodemailer.js
@@ -1,22 +1,21 @@
 import nodeMailer from "nodemailer";
+const resend = new Resend(process.env.SMTP_PASS);
+// const trasporter = nodeMailer.createTransport({
+// host: process.env.SMTP_HOST,
+//   port: process.env.SMTP_PORT,
+//   secure: false, // Use true for port 465, false for port 587
+//   auth: {
+//     user: process.env.SMTP_USER,
+//     pass: process.env.SMTP_PASS,
+//   },
+// });
 
-console.log(process.env.SMTP_USER,process.env.SMTP_PASS)
-const trasporter = nodeMailer.createTransport({
-host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
-  secure: false, // Use true for port 465, false for port 587
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
-  },
-});
+// trasporter.verify((error, success) => {
+//   if (error) {
+//     console.log("SMTP VERIFY ERROR ❌", error);
+//   } else {
+//     console.log("SMTP SERVER READY ✅");
+//   }
+// });
 
-trasporter.verify((error, success) => {
-  if (error) {
-    console.log("SMTP VERIFY ERROR ❌", error);
-  } else {
-    console.log("SMTP SERVER READY ✅");
-  }
-});
-
-export default trasporter;
+export default resend;
